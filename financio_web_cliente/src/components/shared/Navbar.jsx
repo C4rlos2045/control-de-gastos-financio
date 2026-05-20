f7ba4f8997310afa7ddd10d40029e5dfa0109728
Juan <carlos.sanchez270904@gmail.com>
2026-05-20 16:24:30 -0600

    Inicializacion del proyecto y creacion de carpetas para componentes, paginas, estilos, servicios, etc

---
+++ financio_web_cliente/src/components/shared/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+return (
+    <header className="navbar">
+
+    <div className="navbar__logo">
+        FinanCIO
+    </div>
+
+    <nav className="navbar__nav">
+        <a href="#">Inicio</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Movimientos</a>
+    </nav>
+    </header>
+);
+}
+
+export default Navbar;
---
+++ financio_web_cliente/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/shared/Navbar';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Bienvenido al sistema</h1>
+      </main>
+    </>
+  );
+}
+
+export default Home;
